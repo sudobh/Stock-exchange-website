6e377a8a153b5f49654d084552a054df3bc0e08f
sudobh <subodhsingh2030@gmail.com>
2024-05-07 14:54:21 +0530

    add auth

---
+++ backend/src/handlers/users.ts
@@ -0,0 +1,49 @@
+import { error } from "console";
+import prisma from "../db";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth"
+import { json } from "stream/consumers";
+
+export const createNewUser = async (req, res) => {
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                username: req.body.username,
+                password: await hashPassword(req.body.password),
+                demat: req.body.demat,
+                email: req.body.email
+            }
+        })
+
+        const token = createJWT(user)
+        res.json({ token })
+    }
+    catch (e){
+        console.log(e)
+        res.status(400)
+        res.json({error: e})
+    }
+}
+
+export const signIn = async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique ({
+            where: {
+                username: req.body.username
+            }
+        })
+        const isValid = await comparePasswords(req.body.password, user.password)
+        if (!isValid){
+            res.status(401)
+            res.json({message: "invalid password"})
+            return
+        }
+        const token = createJWT(user)
+        res.json({token})
+        }
+    catch (e){
+        console.log(e)
+        res.status(400)
+        res.json({error: "couldn't sign in due to some error"})
+    }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,24 @@
-import express from 'express'
+import express from "express"
+import router from "./router"
+import morgan from "morgan"
+import cors from "cors"
+import { protect } from "./modules/auth"
+import { createNewUser, signIn } from "./handlers/users"
+
 const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.status(200)
     res.json({message: "hello"})
 })
+
+app.use("/api",protect, router)
+app.post("/user", createNewUser)
+app.post("/signin", signIn)
 
 export default app
